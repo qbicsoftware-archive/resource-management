ca185e91cc1faf4e4800cff93497e1611f784eda
David Wojnar <david.wojnar@uni-tuebingen.de>
2015-09-16 16:43:36 +0200

    added tests for using apache velocity and creating pdf files

--- test/facs/test/TestVelocity.java
+++ test/facs/test/TestVelocity.java
@@ -2,11 +2,17 @@ package facs.test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -55,8 +61,41 @@ public class TestVelocity {
       context.put("postalcode", "D-12345");
       context.put("city", "Berlin");
       
+      context.put("sender_name", "Dr. Stella Autenrieth");
+      context.put("sender_function","Geschaeftsfuehrerin");
       context.put("sender_postalcode", "sender postal");
       context.put("sender_city", "davidcity");
+      context.put("sender_street", "Auf der Morgenstelle 42");
+      context.put("sender_phone","+49-7071-29-72163");
+      context.put("sender_email","qbic@qbic.uni");
+      context.put("sender_url","qbic.uni-tuebingen.de");
+      context.put("sender_faculty","Medizinischen Fakultät");
+      context.put("project_description","Dieses Angebot beinhaltet die Etablierungsarbeiten für die proteomeomische Analyse von Formalin-fixed, paraffin-embedded (FFPE) Gewebe. Desweiteren beinhaltet das Angebot die Kosten für proteomische Analysen für insgesamt 12 Proben (4 Herzen mit je 3 Bereiche). Im Vorfeld werden Metadaten zu den jeweiligen Proben ausgetauscht. Die Proben werden dann an das Labor zur massenspektrometrischen Messung durchgereicht. Das Angebot umfasst massenspektrometrische Analysen des Proteoms, sowie die bioinformatische Auswertung und eine Langzeitspeicherung der Rohdaten und Resultate.");
+      context.put("project_short_description", "FFPE Proteomics: Methodenetablierung und Experiment");
+      context.put("project_number", "QA2014016");
+      
+      ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+      HashMap<String,String> map = new HashMap<String,String>();
+      map.put("date", "21.12.2015");
+      map.put("time_frame", "40:20h");
+      map.put("description", "blablablabalbalbalbal");
+      map.put("cost", "3.50");
+      list.add( map );
+    
+      map =  new HashMap<String,String>(); 
+      map.put("date", "21.12.2015");
+      map.put("time_frame", "40:20h");
+      map.put("description", "blablablabalbalbalbal");
+      map.put("cost", "3.50");
+      list.add( map );
+      map = new HashMap<String,String>();
+      map.put("date", "21.11.2015");
+      map.put("time_frame", "00:20h");
+      map.put("description", "for real this will be a longer exxample. I am a veeeerrrryyyyyy llooooonnnnggggg   description, that basically describes nothing for real!!!");
+      map.put("cost", "1.50");
+      list.add( map );
+      context.put("costs", list);
+      context.put("total_cost", "512");
       
       File file = new File(Paths.get(System.getProperty("user.dir"), "test/facs/test/templates", "test.tex").toFile().toString());
       // if file doesnt exists, then create it
@@ -68,9 +107,37 @@ public class TestVelocity {
       
       t.merge(context, bw);
       bw.close();
+      
+      //create pdf
+      List<String> cmd = new ArrayList<String>(
+          Arrays.asList("pdflatex","-interaction=nonstopmode", file.getName()));
+      File resultFile = Paths.get(file.getParent(),"test.pdf").toFile();
+      //Runtime rt = Runtime.getRuntime();
+      //Process p = rt.exec(cmd);
+      ProcessBuilder pb = new ProcessBuilder(cmd);
+      pb.directory(file.getParentFile());
+      Process p = pb.start();
+      
+      int exitValue = p.waitFor();
+      System.out.println(exitValue);
+      if(exitValue != 0 && !resultFile.exists()){
+        StringBuffer sb = new StringBuffer();
+        BufferedReader reader = 
+            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+       String line = "";           
+       while ((line = reader.readLine())!= null) {
+       sb.append(line + "\n");
+       }
+        System.out.println(sb.toString());
+        fail();
+      }
+      
+      
     } catch (Exception e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
+      
       fail();
     }
   }
